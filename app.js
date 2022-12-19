5121d41397a620095ec3d5aac3802376462118d1
Oscar Ballesta <oscarbc@digitalware.com.co>
2022-12-19 09:52:18 -0500

    Add get generic

--- app.js
+++ app.js
@@ -26,7 +26,21 @@ const connection = mysql.createConnection({
 
 //---------------------------------------------------------------------------------------------------------
 // GET
-app.get('/api/estudiantes',cors(), (req, res) => {
+
+
+app.get('/api/arrendadores',cors(), (req, res) => {
+    const sql = `SELECT * FROM Arrendador`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
+
+app.get('/api/comentarios',cors(), (req, res) => {
     const sql = `SELECT * FROM Estudiante`;
     connection.query(sql, (error, result) => {
         if (error) throw error;
@@ -50,6 +64,55 @@ app.get('/api/estudiantes',cors(), (req, res) => {
     });
 });
 
+
+app.get('/api/facturas',cors(), (req, res) => {
+    const sql = `SELECT * FROM Factura`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
+
+
+app.get('/api/habitaciones',cors(), (req, res) => {
+    const sql = `SELECT * FROM Habitacion`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
+
+app.get('/api/modopago',cors(), (req, res) => {
+    const sql = `SELECT * FROM Modo_pago`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
+
+app.get('/api/reserva',cors(), (req, res) => {
+    const sql = `SELECT * FROM Reserva`;
+    connection.query(sql, (error, result) => {
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.send('Not result');
+        }
+    });
+});
 
 
 app.listen(3000, () => {
